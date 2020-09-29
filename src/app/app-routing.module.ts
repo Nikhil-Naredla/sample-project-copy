843d78e36aec89b6efe2c7304cae070064ee7678
Nikhil-Naredla <naredlanikhil.nn@gmail.com>
2020-09-29 20:02:23 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlertsViewComponent } from './components/alerts-view/alerts-view.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { DigitalThreadsComponent } from './components/digital-threads/digital-threads.component';
+import { HomeComponent } from './components/home/home.component';
+import { KanbanViewComponent } from './components/kanban-view/kanban-view.component';
+import { MachineViewComponent } from './components/machine-view/machine-view.component';
+import { ProductionReportsComponent } from './components/production-reports/production-reports.component';
+import { SummaryViewComponent } from './components/summary-view/summary-view.component';
+import { SupportComponent } from './components/support/support.component';
+import { TutorialsDocumentsComponent } from './components/tutorials-documents/tutorials-documents.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",component :HomeComponent
+},{
+  path:"summary-view",component :SummaryViewComponent
+},
+{
+  path:"kanban-view",component :KanbanViewComponent
+},{
+  path:"Machine-view",component :MachineViewComponent
+},{
+  path:"Production-Reports",component :ProductionReportsComponent
+},{
+  path:"Digital-Threads",component :DigitalThreadsComponent
+},{
+  path:"Alerts-view",component :AlertsViewComponent
+},
+{
+  path:"Announcements",component :AnnouncementComponent
+},
+{
+  path:"Support",component :SupportComponent
+},{
+  path:"Tutorials&Documents",component :TutorialsDocumentsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryViewComponent } from './components/summary-view/summary-view.component';
+import { KanbanViewComponent } from './components/kanban-view/kanban-view.component';
+import { MachineViewComponent } from './components/machine-view/machine-view.component';
+import { ProductionReportsComponent } from './components/production-reports/production-reports.component';
+import { DigitalThreadsComponent } from './components/digital-threads/digital-threads.component';
+import { AlertsViewComponent } from './components/alerts-view/alerts-view.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MessageComponent } from './components/message/message.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { SupportComponent } from './components/support/support.component';
+import { TutorialsDocumentsComponent } from './components/tutorials-documents/tutorials-documents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryViewComponent,
+    KanbanViewComponent,
+    MachineViewComponent,
+    ProductionReportsComponent,
+    DigitalThreadsComponent,
+    AlertsViewComponent,
+    NavbarComponent,
+    HomeComponent,
+    MessageComponent,
+    AnnouncementComponent,
+    SupportComponent,
+    TutorialsDocumentsComponent
   ],
   imports: [
     BrowserModule,
